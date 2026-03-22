ba8973702528a94dcfe5b59a26d36b9689010200
Baran Daşdemir <76569348+barandasdemir0@users.noreply.github.com>
2026-03-22 20:58:59 +0300

    temizleme

--- Frontend/WebUILayer/Areas/Admin/Services/Abstract/ITopicApiService.cs
+++ Frontend/WebUILayer/Areas/Admin/Services/Abstract/ITopicApiService.cs
@@ -4,8 +4,7 @@ namespace WebUILayer.Areas.Admin.Services.Abstract
 {
     public interface ITopicApiService:IGenericApiService<TopicDto,CreateTopicDto,UpdateTopicDto>
     {
-        //Task<UpdateTopicDto> GetTopicForEditAsync();
-        //Task SaveTopicAsync(UpdateTopicDto updateTopicDto);
+     
         Task<List<TopicDto>> GetAllAdminAsync();
         Task RestoreAsync(Guid guid);
     }
