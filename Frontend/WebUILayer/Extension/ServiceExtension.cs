24a7a97a76604ae0cde0a4941df1d372bcae2a15
Baran Daşdemir <76569348+barandasdemir0@users.noreply.github.com>
2026-05-08 00:18:13 +0300

    optimizasyon için google botlarının preloaderı görmesini engelledim

--- Frontend/WebUILayer/Extension/ServiceExtension.cs
+++ Frontend/WebUILayer/Extension/ServiceExtension.cs
@@ -254,14 +254,14 @@ public static class ServiceExtension
                 "/Mytheme/portfolio/css/responsive.css"
             );
 
-            //// JS dosyalarını ayrı ayrı küçült (bundle yapmaya gerek yok)
-            pipeline.MinifyJsFiles("/Mytheme/portfolio/js/animations.js",
-                                   "/Mytheme/portfolio/js/main.js",
-                                   "/Mytheme/portfolio/js/github.js",
-                                   "/Mytheme/portfolio/js/language.js",
-                                   "/Mytheme/portfolio/js/chatbot.js",
-                                   "/Mytheme/portfolio/js/theme.js",
-                                   "/js/site.js");
+            ////// JS dosyalarını ayrı ayrı küçült (bundle yapmaya gerek yok)
+            //pipeline.MinifyJsFiles("/Mytheme/portfolio/js/animations.js",
+            //                       "/Mytheme/portfolio/js/main.js",
+            //                       "/Mytheme/portfolio/js/github.js",
+            //                       "/Mytheme/portfolio/js/language.js",
+            //                       "/Mytheme/portfolio/js/chatbot.js",
+            //                       //"/Mytheme/portfolio/js/theme.js",
+            //                       "/js/site.js");
         });
     }
 
