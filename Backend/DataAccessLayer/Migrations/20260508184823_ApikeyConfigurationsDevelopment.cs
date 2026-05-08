8f755f3e175eabd4d58e679b36fb97ec5b298970
Baran Daşdemir <76569348+barandasdemir0@users.noreply.github.com>
2026-05-08 21:51:20 +0300

    migration yapıldı

---
+++ Backend/DataAccessLayer/Migrations/20260508184823_ApikeyConfigurationsDevelopment.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApikeyConfigurationsDevelopment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApiKey",
+                table: "ChatbotSettings",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApiKey",
+                table: "ChatbotSettings",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+        }
+    }
+}
