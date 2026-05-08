21a33352d1ec884643c903e9ee07782aea5188d3
Baran Daşdemir <76569348+barandasdemir0@users.noreply.github.com>
2026-05-08 10:27:14 +0300

    testimonials ana sayfada gözükmeme sorunu düzeltildi

--- Frontend/WebUILayer/Services/Concrete/PublicGuestBookApiService.cs
+++ Frontend/WebUILayer/Services/Concrete/PublicGuestBookApiService.cs
@@ -9,10 +9,13 @@ public class PublicGuestBookApiService : PublicReadApiService<GuestBookDto>, IPu
     {
     }
 
-    //public async Task<List<GuestBookDto>> GetLatestAsync(int count)
-    //{
-    //    var response = await _httpClient.GetAsync($"api/guestbooks/latest/{count}");
-    //    response.EnsureSuccessStatusCode();
-    //    return await response.Content.ReadFromJsonAsync<List<GuestBookDto>>() ?? new();
-    //}
+    public async Task<List<GuestBookDto>> GetLatestAsync(int count)
+    {
+        var response = await _httpClient.GetAsync($"{_endpoint}/latest/{count}");
+        if (!response.IsSuccessStatusCode)
+        {
+            return new List<GuestBookDto>();
+        }
+        return await response.Content.ReadFromJsonAsync<List<GuestBookDto>>() ?? new();
+    }
 }
